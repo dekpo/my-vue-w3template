e5684410bd6faeb1afdb647b52c2bed6c92c2dec
Dekpo <dekpo@me.com>
2022-08-31 14:14:21 +0200

    Added a slider and some correct JavaScript calls ...

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,16 @@
 <script>
+  import SlideShow from './components/SlideShow.vue'
 
+  export default{
+    data(){
+      return{
+        
+      }
+    },
+    components:{
+      SlideShow
+    }
+  }
 </script>
 
 <template>
@@ -31,5 +42,20 @@
   <a href="#" class="w3-bar-item w3-button w3-padding-large" onclick="myFunction()">MERCH</a>
 </div>
 <!-- End Navbar -->
+<!-- Page content -->
+<div class="w3-content" style="max-width:2000px;margin-top:46px">
+  <SlideShow />
 
+</div>
+<!-- End Page content -->
+<!-- Footer -->
+<footer class="w3-container w3-padding-64 w3-center w3-opacity w3-light-grey w3-xlarge">
+  <i class="fa fa-facebook-official w3-hover-opacity"></i>
+  <i class="fa fa-instagram w3-hover-opacity"></i>
+  <i class="fa fa-snapchat w3-hover-opacity"></i>
+  <i class="fa fa-pinterest-p w3-hover-opacity"></i>
+  <i class="fa fa-twitter w3-hover-opacity"></i>
+  <i class="fa fa-linkedin w3-hover-opacity"></i>
+  <p class="w3-medium">Powered by <a href="https://www.w3schools.com/w3css/default.asp" target="_blank">w3.css</a></p>
+</footer>
 </template>
